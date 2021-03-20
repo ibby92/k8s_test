44d0f787832146839b35e36a37db709a6743ae0e
Ibrahim <m99hassa@uwaterloo.ca>
2021-03-20 14:27:22 -0400

    Update branch

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ docker push ibbyh92/multi-client:$SHA
 docker push ibbyh92/multi-server:$SHA
 docker push ibbyh92/multi-worker:$SHA
 
-kubectl apply -f k8s
+kubectl apply -f k8s/
 kubectl set image deployments/server-deployment server=ibbyh92/multi-server:$SHA
 kubectl set image deployments/client-deployment client=ibbyh92/multi-client:$SHA
 kubectl set image deployments/worker-deployment worker=ibbyh92/multi-worker:$SHA
